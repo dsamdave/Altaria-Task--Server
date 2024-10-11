7fa34cecc658993570f7acf5697a0dafce47c74c
David Sampson <davidchosen33@gmail.com>
2024-10-11 11:40:19 +0100

    updated

--- src/routes/paymentRoutes.ts
+++ src/routes/paymentRoutes.ts
@@ -10,9 +10,9 @@ import paymentCtrl from '../contollers/payment';
 
 const router = express.Router()
 
-router.post('/create-payment-intent', auth, paymentCtrl.createPaymentIntent);
+router.post('/create-payment-intent', paymentCtrl.createPaymentIntent);
 
-router.get('/verify-payment/:paymentIntentId', auth, paymentCtrl.verifyPayment);
+router.get('/verify-payment/:paymentIntentId', paymentCtrl.verifyPayment);
 
 
 
