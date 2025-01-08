ee0d29c633a099385d959a0c1aaa54bd2970fc5d
David Sampson <davidchosen33@gmail.com>
2025-01-09 00:37:05 +0100

    updated

--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -8,7 +8,7 @@ const router = express.Router()
 
 
 router.post('/event',  auth, validateAddEvents, eventCtrl.addEvent);
-router.get('/event',   eventCtrl.getEventsByLocation);
+router.post('/events',   eventCtrl.getEventsByLocation);
 router.post('/bookmark-event/:eventId',   auth, eventCtrl.bookmarkEvents);
 
 router.get('/bookmarked-events',   auth, eventCtrl.getBookmarked);
